413652b97c4805061cf6d7d757c043bbd5ba275f
Florian <florian.gau@epitech.eu>
2019-03-04 13:47:51 +0100

    error gestion strchr

--- src/strchr.asm
+++ src/strchr.asm
@@ -13,11 +13,17 @@ strchr:
 	jmp loop_start
 	ret
 
-loop_start:	
+loop_start:
+	cmp byte [rdi + rax], 0
+		je NULL
 	cmp byte [rdi + rax], sil
 		je RETURN
 	add rax, 1
 	jne	loop_start
+
+NULL:
+	mov rax, 0
+	ret
 
 RETURN:
 	add rax, rdi
