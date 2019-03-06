349fe9a406487fc1b8193315f0ca96550ecc3c81
Florian <florian.gau@epitech.eu>
2019-03-06 13:58:04 +0100

    try to have 100 strchr

--- src/strchr.asm
+++ src/strchr.asm
@@ -8,16 +8,14 @@ strchr:
 	mov rax, 0
 	cmp rdi, 0
 		je RETURN
-	cmp rsi, 0
-		je RETURN
 	jmp loop_start
 	ret
 
 loop_start:
-	cmp byte [rdi + rax], 0
-		je NULL
 	cmp byte [rdi + rax], sil
 		je RETURN
+	cmp byte [rdi + rax], 0
+		je NULL
 	add rax, 1
 	jne	loop_start
 
